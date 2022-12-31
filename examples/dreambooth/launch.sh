d06f6e42ca9780998b385cdfea9b5c1fd7e8413d
Sita Bérété <sita.berete.3@gmail.com>
2022-12-31 09:43:09 +0000

    Update launch.sh

--- examples/dreambooth/launch.sh
+++ examples/dreambooth/launch.sh
@@ -1,7 +1,7 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export INSTANCE_DIR="/workspace/training_data/subject1"
 export OUTPUT_DIR="/workspace/models/subject1"
-export CLASS_DIR="/workspace/regularisations"
+export CLASS_DIR="/workspace/regularisations/man"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -9,7 +9,7 @@ accelerate launch train_dreambooth.py \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
   --class_data_dir=$CLASS_DIR \
-  --instance_prompt="a photo of sbjI person" \
+  --instance_prompt="a photo of sbjI" \
   --revision="fp16" \
   --with_prior_preservation --prior_loss_weight=1.0 \
   --seed=3434554 \
@@ -22,9 +22,9 @@ accelerate launch train_dreambooth.py \
   --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=50 \
+  --num_class_images=200 \
   --sample_batch_size=4 \
   --max_train_steps=2000 \
   --save_interval=400 \
-  --save_sample_prompt="a photo of sbjI person" \
-  --class_prompt="a photo of person" \
+  --save_sample_prompt="a photo of sbjI" \
+  --class_prompt="a photo of man" \
