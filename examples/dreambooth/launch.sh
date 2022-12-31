061628072755948c10ec27f6bb66134fca062fa1
Sita Bérété <sita.berete.3@gmail.com>
2022-12-31 12:17:13 +0000

    Update launch.sh

--- examples/dreambooth/launch.sh
+++ examples/dreambooth/launch.sh
@@ -22,7 +22,7 @@ accelerate launch train_dreambooth.py \
   --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=200 \
+  --num_class_images=50 \
   --sample_batch_size=4 \
   --max_train_steps=2000 \
   --save_interval=400 \
