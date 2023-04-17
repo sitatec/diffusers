4888656350035b2c171fde6207b2c39b21713e8f
Sita Bérété <sita.berete.3@gmail.com>
2023-04-17 02:03:16 +0000

    Update launch_flax.sh

--- examples/dreambooth/launch_flax.sh
+++ examples/dreambooth/launch_flax.sh
@@ -2,7 +2,7 @@ export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export INSTANCE_DIR="~/training_data/subject1"
 export OUTPUT_DIR="~/models/subject1"
 
-python train_dreambooth_flax.py \
+python3 train_dreambooth_flax.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
   --train_text_encoder \
